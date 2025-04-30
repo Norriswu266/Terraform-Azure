a7e2feb30cce2acfe95726c9db564ab04ab72d49
fan630 <48875309+fan630@users.noreply.github.com>
2025-04-30 14:45:49 +0800

    fix: remove client secret from code

---
+++ modules/tenants/tenant-norris/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+ alias           = "tenant_norris"
+ subscription_id = "894b3de8-c74a-4759-9639-ebaae15d2a48"
+ tenant_id       = "f1227eab-e2cf-4c66-a910-b8c3b5db34a8"
+ features        {}
+}
+
+module "tenant_norris" {
+  source = "../../windows_vm"
+  providers = {
+    azurerm = azurerm.tenant_norris
+  }
+  tenant_name         = "tenant-norris"
+  location            = "East Asia"
+  resource_group_name = "tenant-norris-rg"
+  subscription_id = "894b3de8-c74a-4759-9639-ebaae15d2a48"
+  tenant_id       = "f1227eab-e2cf-4c66-a910-b8c3b5db34a8"
+}
+
+output "public_ip" {
+  value = module.tenant_norris.public_ip
+}
+
+output "private_ip" {
+  value = module.tenant_norris.private_ip
+}
+
+
